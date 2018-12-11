b32787e8bd7259e2932c06345ff9c19dffef257d
j-yama <j-yama@example.com>
2018-12-12 01:26:31 +0900

    シンプルな検索UIを実装した

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ReactiveBase, DataSearch, ResultList, SelectedFilters } from '@appbaseio/reactivesearch';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="main-container">
+        <ReactiveBase
+          app="steam-search"
+          url="http://localhost:9200"
+        >
+          <DataSearch
+            componentId="title"
+            dataField={["ResponseName"]}
+            queryFormat="and"
+          />
+          <SelectedFilters />
+          <ResultList
+            componentId="resultLists"
+            dataField="ResponseName"           
+            size={10}
+            pagination={true}
+            react={{
+              "and": ["title"]
+            }}
+            onData={(res) => {
+              return {
+                title: res.ResponseName,
+              }
+            }}
+          />
+        </ReactiveBase>
+      </div >
     );
   }
 }
-
 export default App;
