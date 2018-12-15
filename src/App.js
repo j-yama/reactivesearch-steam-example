2953957b6e8a982ea305fe12bfbc03788410d4d2
j-yama <j-yama@example.com>
2018-12-15 11:30:39 +0900

    CSSで見た目を調整した

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ReactiveBase, DataSearch, ResultList, SelectedFilters } from '@appbaseio/reactivesearch';
 import './App.css';
+import './SteamSearch.css'
 class App extends Component {
   render() {
     return (
@@ -8,17 +9,39 @@ class App extends Component {
         <ReactiveBase
           app="steam-search"
           url="http://localhost:9200"
+          theme={
+            {
+              typography: {
+                fontFamily: 'Arial, Helvetica, sans-serif',
+                fontSize: '16px',
+              },
+              colors: {
+                titleColor: '#c7d5e0',
+                textColor: '#c7d5e0',
+                backgroundColor: '#212121',
+                primaryColor: '#2B475E',
+              }
+            }
+          }
         >
           <DataSearch
             componentId="title"
             dataField={["ResponseName"]}
             queryFormat="and"
+            placeholder="enter search term"
+            showIcon={false}
+            title="Steam Search"
+            className="data-search"
+            innerClass={{
+              input: 'input',
+              list: 'list',
+            }}
           />
           <SelectedFilters />
           <ResultList
             componentId="resultLists"
-            dataField="ResponseName"           
-            size={10}
+            dataField="ResponseName"
+            size={25}
             pagination={true}
             react={{
               "and": ["title"]
@@ -39,6 +62,13 @@ class App extends Component {
               { label: "Lowest Price", dataField: "PriceInitial", sortBy: "asc" },
               { label: "Highest Price", dataField: "PriceInitial", sortBy: "desc" },
             ]}
+            className="result-list"
+            innerClass={{
+              resultsInfo: "resultsInfo",
+              resultStats: "resultStats",
+              listItem: "listItem",
+              image: "image",
+            }}
           />
         </ReactiveBase>
       </div >
